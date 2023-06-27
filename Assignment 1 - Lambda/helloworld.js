e4dd8cad055c6275191c780f8d613aeb93030e31
Victor Busk <victorbusk@Victors-MacBook-Air.local>
2023-06-27 09:30:06 +0300

    Updated hello world

--- Assignment 1 - Lambda/helloworld.js
+++ Assignment 1 - Lambda/helloworld.js
@@ -1,5 +1,11 @@
-export const handler = async(event) => {
-    console.log('event', event)
+console.log('Loading function');
+
+exports.handler = async (event, context) => {
+    //console.log('Received event:', JSON.stringify(event, null, 2));
+    console.log('value1 =', event.key1);
+    console.log('value2 =', event.key2);
+    console.log('value3 =', event.key3);
+
     const response = {
         statusCode: 200,
         body: JSON.stringify('Hello from Lambda!: JWT secur'),
